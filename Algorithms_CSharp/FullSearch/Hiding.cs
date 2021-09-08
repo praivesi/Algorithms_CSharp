47f7fba16a068d581b3261e2d4a9d1e5ccc3e1f2
praivesi <praivesi@gmail.com>
2021-09-09 00:57:54 +0900

    resolve Hiding in FullSearch (BAEKJOON 1697)

---
+++ Algorithms_CSharp/FullSearch/Hiding.cs
@@ -0,0 +1,69 @@
+﻿namespace Algorithms_CSharp.FullSearch
+{
+    using System;
+    using System.Collections.Generic;
+
+    class Loc
+    {
+        public int pos;
+        public int depth;
+
+        public Loc(int pos, int depth)
+        {
+            this.pos = pos;
+            this.depth = depth;
+        }
+    }
+
+    class Hiding
+    {
+        static bool[] visited;
+
+        static void Main(string[] args)
+        {
+            string line = Console.ReadLine();
+            var inputs = line.Split(' ');
+
+            int n = int.Parse(inputs[0]);
+            int k = int.Parse(inputs[1]);
+
+            visited = new bool[200001];
+            for (int i = 0; i < 200001; i++)
+            {
+                visited[i] = false;
+            }
+
+            Queue<Loc> q = new Queue<Loc>();
+
+            q.Enqueue(new Loc(n, 0));
+
+            while (q.Count != 0)
+            {
+                var curLoc = q.Dequeue();
+
+                if (curLoc.pos < 0) continue;
+                if (visited[curLoc.pos]) continue;
+
+                visited[curLoc.pos] = true;
+
+                //Console.WriteLine("curLoc.pos = " + curLoc.pos + " / curLoc.depth = " + curLoc.depth);
+
+                if (curLoc.pos == k)
+                {
+                    Console.WriteLine(curLoc.depth);
+
+                    break;
+                }
+
+
+                q.Enqueue(new Loc(curLoc.pos + 1, curLoc.depth + 1));
+                q.Enqueue(new Loc(curLoc.pos - 1, curLoc.depth + 1));
+
+                if (curLoc.pos < k)
+                {
+                    q.Enqueue(new Loc(curLoc.pos * 2, curLoc.depth + 1));
+                }
+            }
+        }
+    }
+}
